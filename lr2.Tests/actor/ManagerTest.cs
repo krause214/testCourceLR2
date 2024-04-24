496729058e672fa429119f667d6065a692ef4d48
Andrey Babincev <andron-2102@mail.ru>
2024-04-24 08:16:33 +0400

    добавил сценарии

--- lr2.Tests/actor/ManagerTest.cs
+++ lr2.Tests/actor/ManagerTest.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using lr2.actor;
+using lr2.entity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace lr2.Tests.actor;
@@ -25,5 +27,30 @@ public class ManagerTest
         Assert.IsNotNull(manager._castingProcess);
         Assert.IsNotNull(manager._forgingProcess);
         Assert.IsNotNull(manager._stampingProcess);
+    }
+    
+    [TestMethod]
+    public void Materials_test()
+    {
+        Manager manager = new Manager();
+        manager._castingProcess.Workers = new List<Worker>();
+        manager._castingProcess.Materials = new List<Material>();
+        manager._castingProcess.AddMaterial(new Material(MaterialQuality.Qualitative));
+        
+        Assert.AreEqual(1, manager._castingProcess.Materials.Count);
+    }
+    
+    [TestMethod]
+    public void Worker_test()
+    {
+        Manager manager = new Manager();
+        manager._castingProcess.Workers = new List<Worker>();
+        manager._castingProcess.AddWorker(new Worker(WorkerType.Caster));
+        
+        Assert.AreEqual(1, manager._castingProcess.Workers.Count);
+        
+        manager._castingProcess.RemoveWorker();
+        
+        Assert.AreEqual(0, manager._castingProcess.Workers.Count);
     }
 }
